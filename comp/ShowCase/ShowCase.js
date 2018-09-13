c021e3db7508f6d537c567d0762f76c549d7e6c3
Brodie Balser <balserbrodie@gmail.com>
2018-09-12 18:39:06 -0600

    made a simple about us set up

--- comp/ShowCase/ShowCase.js
+++ comp/ShowCase/ShowCase.js
@@ -23,7 +23,20 @@ class ShowCase extends React.Component {
 					title: "Project Number B",
 					pic: '/static/img/dog3.jpg',
 					desc: "That guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I am not putting my father in a home! He just came back into my life, and you want to, grab him and, stuff him under a mattress like last month's Victoria's Secret?! Is he keeping his shoulders square? Oooooooh he's tryin'! That just sounds like slavery with extra steps."
-				}, ],
+				},{
+
+					title: "A",
+					pic: '/static/img/dog1.jpg',
+					desc: "That guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I am not putting my father in a home! He just came back into my life, and you want to, grab him and, stuff him under a mattress like last month's Victoria's Secret?! Is he keeping his shoulders square? Oooooooh he's tryin'! That just sounds like slavery with extra steps."
+				},{
+					title: "5",
+					pic: '/static/img/dog2.jpeg',
+					desc: "That guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I am not putting my father in a home! He just came back into my life, and you want to, grab him and, stuff him under a mattress like last month's Victoria's Secret?! Is he keeping his shoulders square? Oooooooh he's tryin'! That just sounds like slavery with extra steps."
+				},{
+					title: "B",
+					pic: '/static/img/dog3.jpg',
+					desc: "That guy is the Red Grin Grumbold of pretending he knows what's going on. Oh you agree huh? You like that Red Grin Grumbold reference? Well guess what, I made him up. You really are your father's children. Think for yourselves, don't be sheep. I am not putting my father in a home! He just came back into my life, and you want to, grab him and, stuff him under a mattress like last month's Victoria's Secret?! Is he keeping his shoulders square? Oooooooh he's tryin'! That just sounds like slavery with extra steps."
+				}],
 			CurrentDisplay: false,
 			SelectedInfo: null,
 			AnimeName: null,
