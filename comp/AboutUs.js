532f00b0bf125a60d2a7130101abbe6222305fb7
Brodie Balser <balserbrodie@gmail.com>
2018-09-15 17:40:35 -0600

    edited positions in aboout us... deploying

--- comp/AboutUs.js
+++ comp/AboutUs.js
@@ -25,14 +25,14 @@ export default () => {
 				#UsImg {
 					width: 30%;
 					border-radius: 40px;
-					margin: 3% 5%;
+					margin: 2% 5%;
 				}
 
 				p {
 					width:50%;
 					position: relative;
 					display: inline-block;
-					bottom: 9vh;
+					bottom: 8vh;
 					font-size: 1.6vw;
 					line-height:40px;
 					letter-spacing: 1.3px;
