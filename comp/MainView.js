0d632520220377f8799aaea56968659edc390575
Brodie Balser <balserbrodie@gmail.com>
2018-09-24 17:57:39 -0600

    added a nice gradient to the main view

--- comp/MainView.js
+++ comp/MainView.js
@@ -22,6 +22,12 @@ export default () => {
 				#MainView {
 					height: 92vh;
 					overflow: hidden;
+
+					//gradient
+					background: -moz-radial-gradient(center, ellipse cover, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.8) 11%, rgba(0,0,0,0) 82%, rgba(0,0,0,0) 100%); /* FF3.6-15 */
+					background: -webkit-radial-gradient(center, ellipse cover, rgba(0,0,0,0.8) 0%,rgba(0,0,0,0.8) 11%,rgba(0,0,0,0) 82%,rgba(0,0,0,0) 100%); /* Chrome10-25,Safari5.1-6 */
+					background: radial-gradient(ellipse at center, rgba(0,0,0,0.8) 0%,rgba(0,0,0,0.8) 11%,rgba(0,0,0,0) 82%,rgba(0,0,0,0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+					filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#cc000000', endColorstr='#00000000',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
 				}
 
 	      #MainImg {
